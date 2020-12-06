f83b0e5d572cb70fcc02faa37e0f5d0969d0f8c3
RuslanVV <ruslangv@codeacademy.edu.az>
2020-12-06 22:30:51 +0400

    Hw3 with method

---
+++ Hw_ruslan/Hw_ruslan/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Hw_ruslan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ///Verilmis x,y,z ededlerinin en boyuyunu console-a yazdiran metod yazin///
+
+
+            FindBigOne(11, 22, 33);
+
+            int x, y, z;
+            string xStr, yStr, zStr;
+            do
+            {
+
+                Console.WriteLine(" X ededini daxil edin:");
+                xStr = Console.ReadLine();
+
+                Console.WriteLine(" Y ededini daxil edin:");
+                yStr = Console.ReadLine();
+
+                Console.WriteLine(" Z ededini daxil edin:");
+                zStr = Console.ReadLine();
+
+
+            } while (!(int.TryParse(xStr, out x) && (int.TryParse(yStr, out y) && (int.TryParse(zStr, out z)))));
+
+
+
+
+        }
+
+        static int FindBigOne(int x, int y, int z)
+        {
+
+            int result;
+
+            if (x > y && x > z)
+                result = x;
+
+            else if (y > x && y > z)
+                result = y;
+
+            else
+                result = z;
+
+            return result;
+
+
+
+        }
+        
+        
+    }
+}
